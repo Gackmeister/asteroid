3de5d2886269c3481d6353fb9358d2c3988ea535
Gackmeister <joelrivington@yahoo.co.uk>
2025-09-03 21:43:35 +0100

    First

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    print("Starting Asteroids! Screen width: 1280 Screen height: 720")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill ("black")
+        pygame.display.flip()
+
+         
+         
+
+
+if __name__ == "__main__":
+    main()
